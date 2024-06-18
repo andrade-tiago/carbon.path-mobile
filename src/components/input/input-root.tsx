34b31e66e8c4cf890d0357e54f2c6c78b4dc0bd5
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-06-18 19:20:41 -0300

    :lipstick: feat(input): restyle

--- src/components/input/input-root.tsx
+++ src/components/input/input-root.tsx
@@ -8,7 +8,7 @@ interface InputRootProps {
 
 export default function InputRoot({ children }: InputRootProps) {
   return (
-    <View className="w-full rounded-lg bg-gray-100 p-4 shadow shadow-gray-600">
+    <View className="w-full rounded-lg bg-white-0 p-4 shadow shadow-gray-600">
       { children }
     </View>
   )
